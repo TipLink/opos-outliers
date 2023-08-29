b0252808f76aa822b584edd61f3553a26b651ea2
Qudo <qudo@matr.world>
2023-08-29 09:00:56 -0700

    banner for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -132,8 +132,8 @@ export default function Home() {
                                 <div className="badge badge-primary mr-2 mb-2 text-white font-space-mono">No Wallet Required</div>
                                 <div className="badge badge-primary text-white font-space-mono">Completely Free</div>
                             </div>
-                            <div className="alert alert-info my-6">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                            <div className="flex bg-info  text-black p-5 rounded-xl my-6">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current shrink-0 w-6 h-6 mr-1"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                                 <span>New attributes available!</span>
                             </div>
                         </div>
